1f8c9c4a05c95eaab52c15ef82295f1b7cc1e5df
alanyeh20001 <alanyeh20001@hotmail.com>
2023-03-02 12:25:14 +0800

    Workaround for git directory ownership issue

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,7 @@
 #!/bin/sh -l
 
+# Workaround for git directory ownership issue with git version after 2.35.2.
+# Issue is similar to https://github.com/actions/checkout/issues/766.
+git config --global --add safe.directory "$GITHUB_WORKSPACE"
+
 eval "$1"
